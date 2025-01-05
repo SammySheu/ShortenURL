250ae1f5557756775be7cf28400622aae8afb056
SammySheu <samsheu1997@gmail.com>
2025-01-05 19:20:59 +0800

    Change db to mysql

--- shortenURL/shortenURL/settings.py
+++ shortenURL/shortenURL/settings.py
@@ -95,8 +95,12 @@ WSGI_APPLICATION = 'shortenURL.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shortenurl',
+        'USER': 'shortenurl_user',
+        'PASSWORD': 'shortenurl_user',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
